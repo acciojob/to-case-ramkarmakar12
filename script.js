26465723c288be211c6b03716bec8c26066d373b
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-01 08:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function toCase(text) {
   // write your code here
+	    if (text.length === 0) return '-';
+    let fin = `${text.toLowerCase()}-${text.toUpperCase()}`;
+    return fin;
 }
 
 // DO not change the code below
